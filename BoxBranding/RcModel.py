a46db6bc001fd8595f91e4ec8ac0610a579e5c19
Captain <openatv@gmail.com>
2024-07-24 17:59:17 +0200

    add gbquad4kpro

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -159,6 +159,7 @@ class RcModel:
 			"gbipbox": "gb0",
 			"gbquad": "gb0",
 			"gbquad4k": "gb3",
+			"gbquad4kpro": "gb5",
 			"gbquadplus": "gb1",
 			"gbtrio4k": "gb4",
 			"gbtrio4kpro": "gb5",
